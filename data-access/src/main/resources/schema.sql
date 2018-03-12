2f36921187c410ba1bd90afa667519644917a33d
Germeau Fabian <fabian.germeau@cetic.be>
2018-03-12 22:08:03 +0100

    Add drop tables to schema.sql

--- data-access/src/main/resources/schema.sql
+++ data-access/src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS t_people;
 CREATE TABLE t_people (
   peopleID      INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   firstname      VARCHAR(255)     NOT NULL,
@@ -5,13 +6,15 @@ CREATE TABLE t_people (
 );
 
 
+DROP TABLE IF EXISTS t_inhabitants;
 CREATE TABLE t_inhabitants (
   inhabitantID  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   peopleID      INT             NOT NULL,
   activated     BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE t_empoyees (
+DROP TABLE IF EXISTS t_employees;
+CREATE TABLE t_employees (
   employeeID    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   peopleID      INT             NOT NULL
 )
