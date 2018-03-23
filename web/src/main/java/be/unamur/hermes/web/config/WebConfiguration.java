5964321914146eb1bc718065a0a03000727ac928
Germeau Fabian <fabian.germeau@cetic.be>
2018-03-23 23:39:37 +0100

    Remove old fashion html pages and transform the web module into a restfull webservice

--- web/src/main/java/be/unamur/hermes/web/config/WebConfiguration.java
+++ web/src/main/java/be/unamur/hermes/web/config/WebConfiguration.java
@@ -10,20 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @ComponentScan(basePackages = {"be.unamur.hermes.web.controller"})
-// Un peut comme les autres module, ici on active l'autoconfiguration du web mvc
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-    }
-
-    @Override
-    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
-        configurer.mediaType("css", new MediaType("text", "css"));
-    }
 
 }
--- web/src/main/java/be/unamur/hermes/web/controller/ApplicationController.java
+++ web/src/main/java/be/unamur/hermes/web/controller/ApplicationController.java
@@ -2,13 +2,12 @@ package be.unamur.hermes.web.controller;
 
 import be.unamur.hermes.business.service.InhabitantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping({"/"})
 public class ApplicationController {
