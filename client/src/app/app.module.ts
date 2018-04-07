54a3ee7ce2cfba3303e10d335ddfd4766bad4dd2
David <david@test.test>
2018-04-07 20:09:14 +0200

    error importing mock auth service solved

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AlertService, AuthenticationService, CitizenService } from './_services/index';
+import { AlertService, AuthenticationService, CitizenService, MockAuthService } from './_services/index';
 import { AlertComponent } from './_directives/index';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { AuthGuard } from './_guards/index';
-import { MockAuthService } from './_guards/index';
 import { CreateUserComponent } from './createuser/createuser.component';
 import { HomeComponent } from './home/index';
 import { JwtInterceptor } from './_helpers/index';
