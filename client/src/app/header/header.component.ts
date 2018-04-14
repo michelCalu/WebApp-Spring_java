d92df9230385068f45556c0a5b4b2939b13689ef
David <david@test.test>
2018-04-14 10:40:05 +0200

    can switch languages

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Observable } from 'rxjs/Observable';
 import { AlertService, MockAuthService } from './../_services/index';
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styles: [
-    `.angular-logo {
+        `.angular-logo {
         margin: 0 4px 3px 0;
         height: 35px;
         vertical-align: middle;
@@ -23,17 +24,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+    // TODO this should be done once, somewhere, for the whole application
+    // language and fallback language
+    currentLanguage: string;
     isLoggedIn$: Observable<boolean>;
 
-    constructor(private mockAuthService: MockAuthService, private alertService: AlertService) { }
+    constructor(private mockAuthService: MockAuthService, private alertService: AlertService,
+        private translateService: TranslateService) { }
 
     ngOnInit() {
         this.isLoggedIn$ = this.mockAuthService.isLoggedIn;
+        // TODO this should be done once, somewhere, for the whole application
+        // language and fallback language
+        this.translateService.setDefaultLang('fr');
+        this.translateService.use('fr');
+        this.currentLanguage = this.translateService.currentLang;
     }
 
-    onLogout(){
+    onLogout() {
         this.mockAuthService.logout();
         this.alertService.success('Vous êtes déconnecté');
+    }
+
+    switchLanguage() {
+        const newLanguage = this.currentLanguage === 'fr' ? 'en' : 'fr';
+        this.translateService.use(newLanguage);
+        this.currentLanguage = newLanguage;
     }
 
 }
