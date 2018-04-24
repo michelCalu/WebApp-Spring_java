125b32447946a1c5e8689e67afc82d521ea5b097
David <david@test.test>
2018-04-24 23:17:57 +0200

    create request with file actually sends to backend

--- client/src/app/_services/request.service.ts
+++ client/src/app/_services/request.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import 'rxjs/add/operator/catch';
 import { CitizenRequest, Citizen } from '../_models';
 import { HttpHeaders } from '@angular/common/http';
 import * as configData from '../configuration-data';
@@ -23,24 +24,10 @@ export class RequestService {
   }
 
   public createRequestWithFileUploads(request: CitizenRequest, files: Array<any> /* file ? */): Observable<boolean> {
-      /*
-        TODO should be: this.http.post('/requests', {request: request, files: files});
-      */
 
-    // TODO to be removed:
-    let existingRequests = JSON.parse(sessionStorage.getItem('requests'));
-    if (!existingRequests) {
-      existingRequests = [];
-    }
-
-    request.status = 'ongoing';
-    request.citizen = new Citizen();
-    request.citizen.firstName = 'John';
-    request.citizen.lastName = 'Doe';
-    existingRequests.push(request);
-
-    sessionStorage.setItem('requests', JSON.stringify(existingRequests));
-    return Observable.of(true);
+    return this.http.post(this.serverAddress + '/requests', {request: request, files: files})
+            .map(res => true)
+            .catch(err => Observable.of(false));
 
   }
 
