0681fb5fe02539eebe13bc33f38ec8b3d34af01b
David <david@test.test>
2018-05-05 04:38:05 +0200

    parking request: display car details

--- client/src/app/parking-request-data/parking-request-data.component.ts
+++ client/src/app/parking-request-data/parking-request-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CitizenRequest  } from '../_models';
 
 @Component({
@@ -6,11 +6,19 @@ import { CitizenRequest  } from '../_models';
     templateUrl: 'parking-request-data.component.html'
 })
 
-export class ParkingRequestDataComponent {
+export class ParkingRequestDataComponent implements OnInit {
 
     @Input()
     request: CitizenRequest;
 
+    carData = new Map<string, Object>();
+
     constructor() { }
+
+    ngOnInit(): void {
+        for (const field of this.request.data) {
+            this.carData[field.code] = field.fieldValue;
+        }
+    }
 
 }
