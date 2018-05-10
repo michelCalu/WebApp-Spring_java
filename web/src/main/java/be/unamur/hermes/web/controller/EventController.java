3cf2e2cfd37eff02e26f3fa98b4f5c1ec70dfad3
Thomas Elskens <thomas.elskens@student.unamur.be>
2018-05-10 22:36:24 +0200

    add optional parameter eventType

--- web/src/main/java/be/unamur/hermes/web/controller/EventController.java
+++ web/src/main/java/be/unamur/hermes/web/controller/EventController.java
@@ -1,6 +1,7 @@
 package be.unamur.hermes.web.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import be.unamur.hermes.dataaccess.entity.Event;
 
 /**
  * Please note this is a read-only controller : event creation is of the entire
- * responbility of the server.
+ * responsibility of the server.
  * 
  * @author Thomas_Elskens
  *
@@ -33,7 +34,10 @@ public class EventController {
     // READ
 
     @GetMapping
-    ResponseEntity<List<Event>> getEvents(@RequestParam("requestId") long requestId) {
+    ResponseEntity<List<Event>> getEvents(@RequestParam("requestId") long requestId,
+	    @RequestParam("eventType") Optional<String> eventType) {
+	if (eventType.isPresent())
+	    return ResponseEntity.ok(eventService.findByReq(requestId, eventType.get()));
 	return ResponseEntity.ok(eventService.findByReq(requestId));
     }
 }
