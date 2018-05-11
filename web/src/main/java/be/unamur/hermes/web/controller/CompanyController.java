f446e9a94009af72a0daabad7c2e786a90b1821e
mca <michel.caluwaerts@student.unamur.be>
2018-05-11 13:58:01 +0200

    added Regex for company check

--- web/src/main/java/be/unamur/hermes/web/controller/CompanyController.java
+++ web/src/main/java/be/unamur/hermes/web/controller/CompanyController.java
@@ -1,7 +1,10 @@
 package be.unamur.hermes.web.controller;
 
+import be.unamur.hermes.business.exception.BusinessException;
 import be.unamur.hermes.business.service.CompanyService;
 import be.unamur.hermes.dataaccess.entity.Company;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import java.net.URI;
 public class CompanyController {
 
     private final CompanyService companyService;
+    private static Logger logger = LoggerFactory.getLogger(CompanyController.class);
 
     @Autowired
     public CompanyController(CompanyService companyService) {
@@ -23,16 +27,21 @@ public class CompanyController {
     // CREATE
     @PostMapping
     public ResponseEntity<Object> create(@RequestBody Company company) {
-	long companyNb = companyService.register(company );
-	URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(companyNb)
-		.toUri();
-	return ResponseEntity.created(location).build();
+        try {
+            companyService.register(company);
+            URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{companyNb}").buildAndExpand(company.getCompanyNb())
+                    .toUri();
+            return ResponseEntity.created(location).build();
+        }catch (BusinessException ex) {
+            logger.error("Create company failed", ex);
+            throw new BusinessException("Create company failed"+ex);
+        }
     }
 
     // READ
     @GetMapping(path = "/{companyNb}")
     public ResponseEntity<Company> findById(@PathVariable(value = "companyNb") String companyNb) {
-	Company company = companyService.findByCompanyNb(companyNb);
-	return ResponseEntity.ok(company);
+        Company company = companyService.findByCompanyNb(companyNb);
+        return ResponseEntity.ok(company);
     }
 }
