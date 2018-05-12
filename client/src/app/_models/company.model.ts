f9fbe9a15215d262efa920f036ea181e54e69d45
David <david@test.test>
2018-05-12 20:45:31 +0200

    create company model entity

---
+++ client/src/app/_models/company.model.ts
@@ -0,0 +1,13 @@
+import { Address } from './address.model';
+
+export class Company {
+  companyNb: number;
+  vatNb: string;
+  address: Address;
+  legalForm: string;
+  contactPerson: number;
+
+  // TODO:
+    // status: string;
+    // name: string;
+}
