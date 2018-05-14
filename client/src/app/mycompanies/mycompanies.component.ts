333eeb7340d656010aa35c365952b7f95e083833
kDegryse <killian.degryse@student.unamur.be>
2018-05-14 21:14:09 +0200

    Company connexion

--- client/src/app/mycompanies/mycompanies.component.ts
+++ client/src/app/mycompanies/mycompanies.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Company } from '../_models/company.model';
 import { CompanyService } from '../_services/company.service';
+import { MockAuthService } from '../_services';
+import { Observable } from 'rxjs/Observable';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
     moduleId: module.id,
@@ -10,7 +13,22 @@ import { CompanyService } from '../_services/company.service';
 
 export class MyCompaniesComponent implements OnInit {
 
+    companies$: Observable<Company[]>;
+    selectedRequest: Company;
+    companiesCreationDates = new Map<number, Date>();
+
+    test: Observable<Date>;
+
+    constructor(private companyService: CompanyService, private authService: AuthenticationService) {}
+
     ngOnInit() {
-        //this.loadAllUsers();
+        const currentUser = this.authService.getCurrentUser();
+        this.companies$ = this.companyService.getCompany(currentUser.id);
+        this.companies$.subscribe(requests => {
+            for (const request of requests) {
+                this.companyService.getCreationDate(request.id)
+                        .subscribe(creationDate => this.companiesCreationDates[request.id] = creationDate);
+            }
+        });
     }
 }
