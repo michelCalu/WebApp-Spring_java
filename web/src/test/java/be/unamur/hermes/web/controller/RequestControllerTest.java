d9fa089a490432c4e8cad9ff13df4cb18e5417ad
Thomas Elskens <thomas.elskens@student.unamur.be>
2018-05-22 23:54:07 +0200

    Add test scope to junit dependency ; first spring-boot integration test

---
+++ web/src/test/java/be/unamur/hermes/web/controller/RequestControllerTest.java
@@ -0,0 +1,46 @@
+package be.unamur.hermes.web.controller;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import be.unamur.hermes.business.service.RequestService;
+import be.unamur.hermes.dataaccess.entity.Request;
+import be.unamur.hermes.web.WebApplication;
+
+@RunWith(SpringRunner.class)
+// @WebMvcTest(RequestController.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = WebApplication.class)
+public class RequestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private RequestService service;
+
+    @Test
+    public void test_getRequests_withDepartmentId() throws Exception {
+	Mockito.when(service.findByDepartmentId(5L)).thenReturn(Collections.singletonList(new Request(1L)));
+	mvc.perform(get("/requests?departmentId=5"). //
+		contentType(MediaType.APPLICATION_JSON)). //
+		andExpect(status().isOk()).//
+		andExpect(jsonPath("$", hasSize(1)));
+    }
+
+}
