91b01e6838d2019597679fe8e3eb4b0987249630
Thomas Elskens <thomas.elskens@student.unamur.be>
2018-05-25 18:07:04 +0200

    Mock user for integration test

--- web/src/test/java/be/unamur/hermes/web/controller/RequestControllerTest.java
+++ web/src/test/java/be/unamur/hermes/web/controller/RequestControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,6 +36,7 @@ public class RequestControllerTest {
     private RequestService service;
 
     @Test
+    @WithMockUser("00000000000_empl")
     public void test_getRequests_withDepartmentId() throws Exception {
 	Mockito.when(service.findByDepartmentId(5L)).thenReturn(Collections.singletonList(new Request(1L)));
 	mvc.perform(get("/requests?departmentId=5"). //
